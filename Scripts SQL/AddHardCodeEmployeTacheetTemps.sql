f3ffccffeccc643cef77a813bc302d1afbb939e2
Antoine Methot <amethot@etu.uqac.ca>
2024-11-12 12:35:40 -0500

    Fixed BD, can add multiple employe a une tache

---
+++ Scripts SQL/AddHardCodeEmployeTacheetTemps.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+INSERT INTO EmployeTache (idEmploye, idTache)
+VALUES
+    (2, 1) ;
+
+INSERT INTO Temps (idEmployeTache, heuresTravaillees, dateTravaille)
+VALUES
+    (2, 1.25, '2024-12-25');
---
+++ Scripts SQL/AddHardCodeTache.sql
@@ -0,0 +1,5 @@
+-- SQLite
+
+INSERT INTO Tache (idProjet, etatTache, tacheParent, nom, dateDebut, dateFin)
+VALUES
+    (1, 1, NULL, 'Tâche 1: Revue de letat de lart', '2024-11-07', '2024-12-31') ;
