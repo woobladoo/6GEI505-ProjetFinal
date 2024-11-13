51bd943e9213cfbee21ed78c04db3775141e7081
Antoine Methot <amethot@etu.uqac.ca>
2024-11-13 09:34:58 -0500

    Documentation Doxygen

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+"""! @brief Application Web pour un gestionnaire de projet et de temps."""
+##
+#@mainpage Page principale
+#
+#@section description_main Description
+#Ceci est une application web pour faire la gestion des projets et du temps mis sur les projets
+#
+#@section notes_main Notes
+#- Fait par Antoine Methot, Maxime Simard et Mayatta Ndiaye. Isaak St-Hilaire pour la Documentation
+#
+# Copyright (c) 2024 A.Methot, M.Simard, M.Ndiaye Tous droits réservés.
+
 from flask import Flask, request, url_for, redirect, render_template, session, flash ##import de la classe flask et de certaines fonctions utiles
 from datetime import date
 import sqlite3
@@ -11,11 +24,23 @@ DB = 'DATA.db'
 users = {"admin": "password123"}
 
 def get_db():                           #connexion à la base de données
+    """!
+    @brief Se connecte ala base de données
+    @return Retourne une connection à la base de données
+    """
     conn = sqlite3.connect(DB)
     conn.row_factory = sqlite3.Row  # Allows us to access rows by column name
     return conn
 
 def check_user_exists(username, password):
+
+    """!
+    @brief Vérifie si un utilisateur existe dans la base de données
+    @param username le courriel d'un employé dans la base de données
+    @param password le mot de passe d'un employé dans la base de données
+    @return Retourne un employé de la base de données
+    """
+
     conn = get_db()
     cursor = conn.cursor()
     
@@ -25,6 +50,12 @@ def check_user_exists(username, password):
     return user is not None
 
 def get_projets():
+
+    """!
+    @brief Se connecte a la base de données et cherche les projets
+    @return Retourne la liste de projets
+    """
+        
     conn = sqlite3.connect(DB) # Connect to DB
     cursor = conn.cursor()
 
@@ -41,6 +72,10 @@ def get_projets():
     return projets
 
 def get_clients():
+    """!
+    @brief Se connecte ala base de données et cherche les clients
+    @return Retourne une liste des clients
+    """
     conn = sqlite3.connect(DB) # Connect to DB
     cursor = conn.cursor()
     query = '''
@@ -54,6 +89,10 @@ def get_clients():
     return clients
 
 def get_gestionnaires():
+    """!
+    @brief Se connecte a la base de données et cherche les gestionnaires
+    @return Retourne une liste des gestionnaires
+    """
     conn = sqlite3.connect(DB) # Connect to DB
     cursor = conn.cursor()
     query = '''
@@ -70,15 +109,27 @@ def get_gestionnaires():
 
 @app.route('/', methods=['GET', 'POST'])        ##Définition de l'url d'accueil
 def home():           ##Fonction pour gérer la réaction de la page d'accueil, le titre est dynamique
+  """!
+  @brief Affiche la page d'acceuil
+  @return affiche la page de connexion
+  """
   return redirect(url_for('login'))
 
-@app.route('/listeProjets', methods=['GET', 'POST'])        ##Définition de l'url d'accueil
+@app.route('/listeProjets', methods=['GET', 'POST'])        ##Définition de l'url de la page de connexion
 def listeProjets():           ##Fonction pour gérer la réaction de la page d'accueil, le titre est dynamique
+  """!
+  @brief Cherche les projets dans la base de données
+  @return affiche la page avec la liste des projets
+  """
   projets = get_projets() #Chercher tous les projets dans table Projets de la BD
   return render_template('listeProjets.html', projets = projets)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """!
+    @brief Cherche les utilisateurs de la BD et compare les courriels et mot de passes avec les données dans BD
+    @return Si les données match le formulaire, retourne la page de listeProjet sinon la meme page de connexion
+    """
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -93,40 +144,70 @@ def login():
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
+    """!
+    @brief affiche un message de deconnexion
+    @return affiche la page de connexion
+    """
     session.pop('username', None)
     flash('You have been logged out.', 'success')
     return redirect(url_for('login'))
 
 @app.route('/profil', methods=['GET', 'POST'])
 def profil():
+    """!
+    @brief Render le template de profil html
+    @return affiche la page profil
+    """
     return render_template('profil.html')
 
 @app.route('/employes', methods=['GET', 'POST'])
 def employes():
+    """!
+    @brief Render le template de employes html
+    @return affiche la page avec la liste employes
+    """
     return render_template('employes.html')
 
 @app.route('/projet', methods=['GET', 'POST'])
 def projet():
+    """!
+    @brief Render le template de projet html
+    @return affiche la page contenant les tâches du projet
+    """
     return render_template('projet.html')
 
 @app.route('/tache', methods=['GET', 'POST'])
 def tache():
+    """!
+    @brief Render le template de taches html
+    @return affiche la page d'une tâche avec les sous-tâches et description
+    """
     return render_template('taches.html')
 
 @app.route('/inscription', methods=['GET', 'POST'])
 def inscription():
+    """!
+    @brief Render le template de inscription html
+    @return affiche la page d'inscription d'un employé
+    """
     return render_template('inscription.html')
 
 @app.route('/add_projet', methods=['GET', 'POST'])
 def add_projet():
+    """!
+    @brief Cherche les clients et les gestionnaires dans la BD pour avoir les données du formulaire d'ajout de projet
+    @return Affiche le formulaire d'ajout de projet
+    """
     clients = get_clients()
     gestionnaires = get_gestionnaires()
-
-
     return render_template('add_projet.html', clients = clients, gestionnaires = gestionnaires)
 
 @app.route('/add_projet_todb', methods=['POST'])
 def add_projet_todb():
+    """!
+    @brief Prend les informations du formulaire et les ajoute à la DB
+    @return redirige vers la page liste de projet
+    """
     # Get form data from the request
     nom = request.form['nom']
     client_id = request.form['Client']
@@ -152,6 +233,11 @@ def add_projet_todb():
 
 @app.route('/delete_projet/<int:projet_id>', methods=['POST'])
 def delete_projet(projet_id):
+    """!
+    @brief Supprime un projet de la BD
+    @param projet_id le ID du projet a supprimer
+    @return redirige vers la page liste projet
+    """
     conn = sqlite3.connect(DB)
     cursor = conn.cursor()
     query = 'DELETE FROM Projet WHERE id = ?'
--- readme.txt
+++ readme.txt
@@ -1,3 +1,26 @@
 Application web de gestion de projet et de gestion de temps
-
 GitHub Username du PROF: laguerin
+
+#############################################################
+
+README.md for 6GEI311 - Lab4
+
+1. Introduction Doxygen
+Doxygen est un logiciel qui permet de créer un html/interface à partir d'une documentation 
+dans un code afin d'avoirdes déscriptions du fonctionnement du code et de le visualiser de 
+manière intuitive et claire
+
+2. Apercu du projet et du code
+Le projet documenté est le __main__.py d'un serveur flask. Nous etions malheureusement dans l'impossibilité de terminer
+la laboratoire 3 du cours 6GEI311 donc nous avons utiliser un projet en cours à documenter. Les fonctions documenté correspondent à
+des fonctions de parcour de base de données et des fonctions d'affichage de page HTML.
+
+3. Explication des commentaires générés
+Les commentaires ne sont pag générés mais plutot organisé de manière claire et précis sur un interface html.
+Les commentaires affiché explique les différentes fonction et parametres  à l'aide des décorateurs @brief, @param, @return.
+Doxygen offre aussi un navigateur pour facilement naviguer les commentaires des fonctions et des variables. 
+
+4. Conclusion sur l'importante de la documentation, différence entre documentation Doxygen et documentation traditionnel.
+La documentation est très importante pour permettre plusieurs personnes à travailler sur un même code et se comprendre. La documentation
+sur Doxygen permet de faciliter le processus de documentation des différentes fonctions afin qu'il soit plus facile de les comprendre que si 
+ils étaient documenter que dans le code avec des commentaires.
